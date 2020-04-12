dabd3d2af74c4708ed295b4a88fea02351439f41
elnathanau1 <elnathanau1@gmail.com>
2020-04-11 23:09:00 -0500

    init

---
+++ src/main/kotlin/proj/eau/Application.kt
@@ -0,0 +1,14 @@
+package proj.eau
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("proj.eau")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
